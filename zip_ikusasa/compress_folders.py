a9d5daf6d8b05e5d9dfd2f81871e449a4f018e38
ThembaMoloi <themba.moloi115@gmail.com>
2024-10-21 11:29:10 +0200

    Update compress_folders.py

--- zip_ikusasa/compress_folders.py
+++ zip_ikusasa/compress_folders.py
@@ -18,17 +18,16 @@ def zip_directory(directory_path, zip_path):
                 print(f' Successfully zipped {directory_path} into {zip_path} ')
                 return True
             else:
-                print(f'Compression falied, could not find {directory_path}')
+                print(f'Compression falied, could not find {directory_path}') #in case the file does not exist in the specified directory
                 return False
     except Exception as e:
         print(f'An unxpected error occured: {e}')
 
-if __name__ == "__main__":
-    print(os.getcwd())
-    directory_path = input('Enter directory path:')
-    zip_path = directory_path+'.zip'
+if __name__ == "__main__":    #run the program
+    directory_path = input('Enter directory path:') #get file path
+    zip_path = directory_path+'.zip' #specify zipped file destination
 
     if zip_directory(directory_path, zip_path):
-        print('Compression complete')
+        print('Compression complete') #upon success communicate message
     else:
-        print('Compression failed')
+        print('Compression failed') #upon failure communicate message
